ff4bdb1e327d0cdc222a94545aa0db045dc443ad
Siva <siva.kb@spritle.com>
2021-06-24 17:36:16 +0530

    Update home_controller.rb

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,4 +3,13 @@ class HomeController < ApplicationController
   def index
     render json { sucess:  true }
   end
+  
+  def test
+    dev = 'siva'
+    if dev == 'siva'
+      puts 'ROR Dev'
+    else if dev == 'SG'
+      puts 'React'
+    end
+  end
 end
