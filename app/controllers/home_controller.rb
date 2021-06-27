4ac5c31b61924903212af4627b0e99d11ef33fa5
Siva <siva.kb@spritle.com>
2021-06-27 18:25:14 +0530

    Sivakb patch 1 (#15)
    * Update home_controller.rb
    
    * syntax issue
    
    * Update README.md
    
    * test
    
    * remove comment

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,4 +3,13 @@ class HomeController < ApplicationController
   def index
     render json { sucess:  true }
   end
+  
+  def test
+    dev = 'siva'
+    if dev == 'siva'
+      puts 'ROR Dev'
+    elsif dev == 'SG'
+      puts 'React JS'
+    end
+  end
 end
